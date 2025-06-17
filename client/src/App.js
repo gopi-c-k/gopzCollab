66176be3207fcb39fd9f6327770fd1d4b97bec57
Gopi C K <gopick2004@gmail.com>
2025-06-18 01:07:53 +0530

    feat: add default route to redirect to Home component

--- client/src/App.js
+++ client/src/App.js
@@ -14,6 +14,7 @@ function App() {
         <Route path="/signin" element={<SignIn/>} />
         <Route path="/signup" element={<SignUp/>} />
         <Route path="/home" element={<Home/>} />
+        <Route path="/" element={<Home/>} />
         <Route path='/notification' element={<Notification/>} />
         <Route path="/editor" element={<TextEditor/>} />
       </Routes>
