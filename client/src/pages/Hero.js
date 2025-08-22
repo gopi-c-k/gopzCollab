f0fd194b59f6d167648cff6f09c62c53c7ec8f71
Gopi C K <131648444+gopi-c-k@users.noreply.github.com>
2025-08-22 21:40:50 +0530

    Update Hero.js

--- client/src/pages/Hero.js
+++ client/src/pages/Hero.js
@@ -47,7 +47,6 @@ function Hero() {
             alt="Logo"
             className="w-10 h-10 mr-2"
           />
-          <h1 className="text-xl font-bold text-blue-600">GopzCollab</h1>
         </div>
         <div className="flex items-center space-x-4">
           <button
